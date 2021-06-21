4a19c0f86b8d0ea923eeb981d78a252020010e40
Jessamarie <jessamarie@users.noreply.github.com>
2021-06-21 00:28:27 -0400

    Broken up home page into smaller components; finished styling menu-item component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import './App.css';
-import './homepage.styles.scss'
 
-import HomePage from './homepage.component';
+import HomePage from './pages/homepage/homepage.component';
 
 function App() {
   return (
---
+++ src/components/directory/directory.component.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import MenuItem from '../menu-item/menu-item.component';
+import { sections } from './directory.data';
+
+import './directory.styles.scss'
+
+class Directory extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            sections: sections
+        }
+    }
+
+    render() {
+        return (
+            <div className='directory-menu'>
+                {
+                    this.state.sections.map(({ id, title, imageUrl, size }) => (
+                        <MenuItem key={id} title={title} imageUrl={imageUrl} size={size}/>
+                    ))}
+            </div>
+        )
+    }
+}
+
+export default Directory;
