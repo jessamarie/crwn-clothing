f176cb10c8f7d181d019b5547d25fcfe8a305d71
Jessamarie <jessamarie@users.noreply.github.com>
2021-06-27 20:49:48 -0400

    finishing sign in handle submit method

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ class App extends React.Component {
               id: snapshot.id,
               ...snapshot.data()
             }
-          }, () => console.log(this.state));
+          });
         });
       } else {
         this.setState({ currentUser: userAuth });
