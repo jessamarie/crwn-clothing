81e47cb00b0823a3f38ea41e63570d3d5ecf25e9
Jessamarie <jessamarie@users.noreply.github.com>
2021-11-21 22:22:21 -0500

    updating application w/ reselect, replaced cart selectors with memoized selectors; added checkout page, checkout item component and updated cart to use selectors from reselect

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -9,6 +9,9 @@ import { ReactComponent as Logo } from '../../assets/crown.svg';
 import './header.styles.scss';
 import CartIcon from '../cart-icon/cart-icon.component';
 import CartDropdown from '../cart-dropdown/cart-dropdown.component';
+import { createStructuredSelector } from 'reselect';
+import { selectCurrentUser } from '../../redux/user/user.selectors';
+import { selectCartHidden } from '../../redux/cart/cart.selectors';
 
 const Header = ({ currentUser, hidden }) => (
     <div className='header'>
@@ -33,9 +36,10 @@ const Header = ({ currentUser, hidden }) => (
     </div>
 )
 
-const mapStateToProps = ({ user: { currentUser }, cart: { hidden }}) => ({
-    currentUser,
-    hidden
+const mapStateToProps = createStructuredSelector({
+    currentUser: selectCurrentUser,
+    hidden: selectCartHidden
 })
+
 
 export default connect(mapStateToProps)(Header);
