14d47a81e3322359d796be70f4ab23ea88fcf624
Jessamarie <jessamarie@users.noreply.github.com>
2021-12-07 22:24:19 -0500

    updated dependencies, updated firebase path for new version, removed unused dependencies

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,58 +1,58 @@
-import firebase from 'firebase/app';
+import firebase from 'firebase/compat/app';
 
 // these are automatically attached to ^ firebase when imported.
-import 'firebase/firestore';
-import 'firebase/auth'
+import 'firebase/compat/firestore';
+import 'firebase/compat/auth';
 
 // Firebase in JavaScript uses public keys, no need to worry about hiding it
 const config = {
-    apiKey: "AIzaSyDfoQxAhlHesxLCLRzN_X8CQUQcxHImNiE",
-    authDomain: "crwn-db-7d5e4.firebaseapp.com",
-    projectId: "crwn-db-7d5e4",
-    storageBucket: "crwn-db-7d5e4.appspot.com",
-    messagingSenderId: "127459944545",
-    appId: "1:127459944545:web:b3e40d1d49519e331da220",
-    measurementId: "G-KHK6YTF78T"
+  apiKey: 'AIzaSyDfoQxAhlHesxLCLRzN_X8CQUQcxHImNiE',
+  authDomain: 'crwn-db-7d5e4.firebaseapp.com',
+  projectId: 'crwn-db-7d5e4',
+  storageBucket: 'crwn-db-7d5e4.appspot.com',
+  messagingSenderId: '127459944545',
+  appId: '1:127459944545:web:b3e40d1d49519e331da220',
+  measurementId: 'G-KHK6YTF78T',
 };
 
 if (!firebase.apps.length) {
-    firebase.initializeApp(config);
+  firebase.initializeApp(config);
 } else {
-    firebase.app(); // if already initialized, use that one
+  firebase.app(); // if already initialized, use that one
 }
 
 export const createUserProfileDocument = async (userAuth, additionalData) => {
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = firestore.doc(`users/${userAuth.uid}`);
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-    const snapshot = await userRef.get();
+  const snapshot = await userRef.get();
 
-    if (!snapshot.exists) {
-        const { displayName, email } = userAuth;
-        const createdAt = new Date();
+  if (!snapshot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
 
-        try {
-            await userRef.set({
-                displayName,
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (err) {
-            console.log('error creating user', err.message);
-        }
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData,
+      });
+    } catch (err) {
+      console.log('error creating user', err.message);
     }
+  }
 
-    return userRef;
-}
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 // Always trigger the google popup whenever we use it for auth and signin
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({ prompt: 'select_account' })
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
--- src/firebase/test.js
+++ src/firebase/test.js
@@ -1,10 +1,13 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/firestore';
 
 const firestore = firebase.firestore();
 
-firestore.collection('users').doc('D0knxvaxKXjh2okgX6qn')
-    .collection('cartItems').doc('7cUhWBOChIuVf4dTiLis');
+firestore
+  .collection('users')
+  .doc('D0knxvaxKXjh2okgX6qn')
+  .collection('cartItems')
+  .doc('7cUhWBOChIuVf4dTiLis');
 
 firestore.doc('/users/D0knxvaxKXjh2okgX6qn/cartItems/7cUhWBOChIuVf4dTiLis');
 firestore.collection('/users/D0knxvaxKXjh2okgX6qn/cartItems/');
